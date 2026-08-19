b908e91e950919112355b29353f64ccb32c5c1e5
Hong Yu <chin.hongyu88@gmail.com>
2026-08-19 23:50:14 +0800

    Add greeting and exit

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,10 +1,19 @@
-public class Duke {
+public class Cookie {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String separator = "____________________________________________________________";
+        String banner = " ██████╗ ██████╗  ██████╗ ██╗  ██╗██╗███████╗\n"
+                + "██╔════╝██╔═══██╗██╔═══██╗██║ ██╔╝██║██╔════╝\n"
+                + "██║     ██║   ██║██║   ██║█████╔╝ ██║█████╗  \n"
+                + "██║     ██║   ██║██║   ██║██╔═██╗ ██║██╔══╝  \n"
+                + "╚██████╗╚██████╔╝╚██████╔╝██║  ██╗██║███████╗\n"
+                + " ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Cookie.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
