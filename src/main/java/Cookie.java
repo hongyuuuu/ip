78523e8ffdd9b59fd7eb28f00a65e66aeead6e24
Hong Yu <chin.hongyu88@gmail.com>
2026-08-20 11:30:55 +0800

    Add support for add, list features

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 /**
  * The main entry point for the Cookie command-line application.
  */
 public class Cookie {
     private static final String SEPARATOR = "____________________________________________________________";
+    private static final ArrayList<String> LST = new ArrayList<>(100);
 
     /** Displays Cookie's greeting and the prompt for the first command. */
     private static void greet() {
@@ -30,6 +31,24 @@ public class Cookie {
         System.out.println(SEPARATOR);
     }
 
+    /** Stores user input and prints the message indicating a successful addition */
+    private static void add(String item) {
+        LST.add(item);
+        System.out.println(SEPARATOR);
+        System.out.println("Added: " + item);
+        System.out.println(SEPARATOR);
+    }
+
+    /** Displays the list of items added by users when users enter {@code list} */
+    private static void list() {
+        System.out.println(SEPARATOR);
+        for (int i = 0; i < LST.size(); i++) {
+            String item = LST.get(i);
+            System.out.println(i+1 + ". " + item);
+        }
+        System.out.println(SEPARATOR);
+    }
+
     /** Reads and responds to commands until the user enters {@code bye}. */
     public static void main(String[] args) {
         greet();
@@ -43,9 +62,12 @@ public class Cookie {
                 break;
             }
 
-            System.out.println(SEPARATOR);
-            System.out.println(command);
-            System.out.println(SEPARATOR);
+            if (command.equals("list")) {
+                list();
+                continue;
+            }
+
+            add(command);
         }
     }
 }
